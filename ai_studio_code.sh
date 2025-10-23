1719fe0da9cc9660a20bdc86f817ce4916c6f22e
Rushikesh k. Pundkar <73341691+rushikesh648@users.noreply.github.com>
2025-10-23 10:37:01 +0530

    Add files via upload

---
+++ ai_studio_code.sh
@@ -0,0 +1,5 @@
+pip install SpeechRecognition gTTS
+# For PyAudio (microphone access), it can be tricky.
+# On Windows: pip install pipwin then pipwin install PyAudio
+# On Linux: sudo apt-get install python3-pyaudio
+# On macOS: brew install portaudio; pip install PyAudio
